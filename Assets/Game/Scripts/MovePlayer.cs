524926f664b85ff5dbb8a47421da72a4da5dd02b
Mercurs <33377392+Mercurs@users.noreply.github.com>
2018-06-20 15:33:22 +0400

    controller player aded

--- Assets/Game/Scripts/MovePlayer.cs
+++ Assets/Game/Scripts/MovePlayer.cs
@@ -3,14 +3,19 @@
 public class MovePlayer : MonoBehaviour {
 
 	[SerializeField] protected float speed = 5.0f;
+	RectTransform js;
 	Rigidbody rb;
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody>();
+		js = GameObject.Find("MobileJoystick").GetComponent<RectTransform>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+		var h = Mathf.Round(js.position.x) - 145;
+		var v = Mathf.Round(js.position.y) - 123;
+
+		rb.AddTorque(new Vector3(v * Time.deltaTime * speed , 0 , -h * Time.deltaTime * speed));
 	}
 }
--- Assets/Game/Scripts/PivotPlayer.cs
+++ Assets/Game/Scripts/PivotPlayer.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PivotPlayer : MonoBehaviour {
+	[SerializeField] [HideInInspector] protected float X , Y , Z;
 	Transform player;
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+		X = transform.localPosition.x;
+		Y = transform.localPosition.y;
+		Z = transform.localPosition.z;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.localPosition = player.transform.localPosition;
+		transform.localPosition = new Vector3(player.transform.localPosition.x + X , player.transform.localPosition.y + Y, player.transform.localPosition.z + Z);
 	}
 }
