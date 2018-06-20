b7ec8e2a50debe0daac6177ccdb9665572bd509e
Mercurs <33377392+Mercurs@users.noreply.github.com>
2018-06-20 15:51:53 +0400

    Aded jump to player

--- Assets/Game/Scripts/MovePlayer.cs
+++ Assets/Game/Scripts/MovePlayer.cs
@@ -3,6 +3,8 @@
 public class MovePlayer : MonoBehaviour {
 
 	[SerializeField] protected float speed = 5.0f;
+	[SerializeField] protected float SpeedJump = 5.0f;
+	[SerializeField] protected bool IsGround = false;
 	RectTransform js;
 	Rigidbody rb;
 	// Use this for initialization
@@ -17,5 +19,20 @@ public class MovePlayer : MonoBehaviour {
 		var v = Mathf.Round(js.position.y) - 123;
 
 		rb.AddTorque(new Vector3(v * Time.deltaTime * speed , 0 , -h * Time.deltaTime * speed));
+	}
+	public void JumpToPlayer() {
+		if (IsGround) {
+			rb.velocity += Vector3.up * SpeedJump;
+		}
+	}
+	protected void OnCollisionEnter (Collision other) {
+		if(other.collider.CompareTag("IsGround")) {
+			IsGround = true;
+		}
+	}
+	protected void OnCollisionExit (Collision other) {
+		if(other.collider.CompareTag("IsGround")) {
+			IsGround = false;
+		}
 	}
 }
