e309fa46aee91ea8af0a548f4e0977e80b244e7d
Mercurs <33377392+Mercurs@users.noreply.github.com>
2018-06-22 14:08:07 +0400

    Добавлен звук падение мячика в воду

--- Assets/Game/Scripts/MovePlayer.cs
+++ Assets/Game/Scripts/MovePlayer.cs
@@ -5,7 +5,7 @@ public class MovePlayer : MonoBehaviour {
 	[SerializeField] protected float SpeedJump = 5.0f;
 	[SerializeField] [HideInInspector] protected bool IsGround = false;
 	[SerializeField] protected  SimpleTouchController leftController;
-	[SerializeField] protected AudioSource IsGroundSound = null;
+	[SerializeField] protected AudioSource IsGroundSound = null , IsWaterSound = null;
 	[SerializeField] protected Transform cam;
 	[SerializeField] protected bool isMobile = false;
 
@@ -41,6 +41,12 @@ public class MovePlayer : MonoBehaviour {
 			this.IsGroundSound.Play();
 		}
 	}
+	protected void OnTriggerEnter (Collider other) {
+		if (other.CompareTag("Water Volume")) {
+			this.IsWaterSound.Play();
+		}
+	}
+
 	protected void OnTriggerStay (Collider other) {
 		if (this.isMobile) {
 			if (other.CompareTag("Water Volume")) {
@@ -56,6 +62,7 @@ public class MovePlayer : MonoBehaviour {
 			this.rb.AddTorque(this.cam.transform.forward * -h * Time.deltaTime * this.speed + this.cam.transform.right * v * Time.deltaTime * this.speed);
 		}
 	}
+
 	protected void OnCollisionExit (Collision other) {
 		if(other.collider.CompareTag("IsGround")) {
 			this.IsGround = false;
