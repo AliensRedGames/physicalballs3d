53f2fc7066441d1a43e43f87f6007f15807141b4
Mercurs <33377392+Mercurs@users.noreply.github.com>
2018-06-22 15:46:41 +0400

    Доработка главного меню

--- Assets/Game/Scripts/ManagerUI.cs
+++ Assets/Game/Scripts/ManagerUI.cs
@@ -1,24 +1,39 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ManagerUI : MonoBehaviour {
-	[SerializeField] protected Animator Settings = null , MainMenu = null;
+	[SerializeField] protected Animator Settings = null , MainMenu = null , SettingsSound = null , SettingsVideo;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	public void showSettings () {
-		MainMenu.SetBool("open" , false);
-		Settings.SetBool("open" , true);
+		this.MainMenu.SetBool("open" , false);
+		this.Settings.SetBool("open" , true);
+		this.Settings.GetComponent<MeshRenderer>().material.color = Color.black;
 	}
 
 	public void hideSettings () {
-		MainMenu.SetBool("open" , true);
-		Settings.SetBool("open" , false);
+		this.MainMenu.SetBool("open" , true);
+		this.Settings.SetBool("open" , false);
 	}
 
 	public void loaddemolvl () {
 		SceneManager.LoadScene(1);
+	}
+
+	public void showSettingsSound () {
+		this.SettingsSound.SetBool("open" , true);
+	}
+	public void showSettingsVideo () {
+		this.SettingsVideo.SetBool("open" , true);
+	}
+
+	public void hideSettingsSound () {
+		this.SettingsSound.SetBool("open" , false);
+	}
+	public void hideSettingsVideo () {
+		this.SettingsVideo.SetBool("open" , false);
 	}
 
 	public void ExitGame () {
