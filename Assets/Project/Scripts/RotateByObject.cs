2c1e6db446f0e69d747511e29b8f1f6d4e07b737
Mercurs <33377392+Mercurs@users.noreply.github.com>
2018-06-24 17:14:09 +0400

    лвл дизайн && меню

---
+++ Assets/Project/Scripts/RotateByObject.cs
@@ -0,0 +1,12 @@
+﻿//ака 47
+using UnityEngine;
+
+public class RotateByObject : MonoBehaviour {
+
+	[SerializeField] protected Vector3 vc3 = new Vector3(0,0,0);
+
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate (this.vc3.x,this.vc3.y,this.vc3.z);
+	}
+}
